5c9ffe17244962c5be51c160ca8d45ca82d428a4
Dan Wooster <60037980+dwoosterup@users.noreply.github.com>
2020-06-05 07:43:00 -0400

    Create Sphere.java

---
+++ Sphere.java
@@ -0,0 +1,35 @@
+// place required comments here 
+
+public class Sphere {
+	// define a double precision variable to hold the Sphere's getDiameter
+
+	// define constructor to accept a double precision parameter and store it in the Sphere's diameter
+	public Sphere(double diameter) {
+
+	}
+
+	// define toString that returns a string with the appropriate format
+	public String toString() {
+
+	}
+
+	// return the Sphere's diameter
+	public double getDiameter() {
+
+	}
+
+	// set the Sphere's diameter with the parameter that is passed in 
+	public void setDiameter(double diameter) {
+		this.diameter = diameter; // note the use of "this" which refers to the object itselt; must be used here to clarify the difference between the parameter called diameter and the instance variable called diameter
+	}
+
+	// return the volume per the formula given
+	public double calcVolume() {
+
+	}
+
+	// return the surface area per the formula given
+	public double calcSurfaceArea() {
+
+	}
+}
